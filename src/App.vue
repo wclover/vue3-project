0739e0799a1f20917af1560a79b5ad1312ff8674
liangJiaHao <liangjiahao@ftl-express.cn>
2022-02-27 14:29:29 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <span class="test">helle</span>
+    <el-button>button</el-button>
+  </div>
+</template>
+
+<style lang="scss">
+div {
+  .test {
+    color: red;
+    font-size: 50px;
+  }
+}
+</style>
